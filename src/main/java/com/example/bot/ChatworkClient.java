7a1c34ef26c7269b66c6a1cf134696b3e54a0eb0
Nguyen Tan Tu <tunt@evolableasia.vn>
2019-11-06 10:41:04 +0700

    fix

--- src/main/java/com/example/bot/ChatworkClient.java
+++ src/main/java/com/example/bot/ChatworkClient.java
@@ -6,14 +6,17 @@ import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.methods.PostMethod;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.*;
-import java.net.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.Collections;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Created by ThanhD
@@ -32,7 +35,7 @@ public class ChatworkClient {
      * PARAM FOR CHAT BOT API
      * DOCS: SimSimi API
      */
-    private static final String CB_API_KEY = "AXN38FpcSNgiJzNwnlnhEKJzdLCHyLPoI+AeTVkZ"; //Replace your Simsimi API Key
+    private static final String CB_API_KEY = "XZHaeVUjjfpBPkhC89jlbdRhcXz25S6kOxY4mvN4"; //Replace your Simsimi API Key
     private static final String CB_API_URL = "http://sandbox.api.simsimi.com/request.p";
 //    private static final String CB_API_URL = "http://sandbox.api.simsimi.com/request.p?key=%KEY%&lc=en&ft=0.0&text=%TEXT%";
     private static final String BOT_ID = "[To:4302388]"; //Replace XXXXX to ID ChatWork of Bot
@@ -54,7 +57,7 @@ public class ChatworkClient {
 
 // Notify to room
         sendMessage(roomId, "TO ALL >>> BOT STARTED!");
-
+        System.out.println("2");
         StringBuilder messReply = new StringBuilder();
         int mentionRoomNum = 0;
         while (!breakFlag) {
@@ -74,6 +77,7 @@ public class ChatworkClient {
                     if (message.body.startsWith(BOT_ID)) {
 // Get message from room of ChatWork
                         String messSend = message.body.substring(message.body.indexOf("\n") + 1);
+
 // Check request BOT stop?
                         if ("STOP".equals(messSend)) {
 // Notify to room
@@ -87,17 +91,25 @@ public class ChatworkClient {
                         }
 
 // Make new message from BOT
-                        messReply.append("\n")
-                                .append("[To:")
-                                .append(message.account.accountId)
-                                .append("] ")
-                                .append(message.account.name)
-                                .append("\n")
-                                .append(getMessageFromBot(messSend));
+                        try {
+                            System.out.println(messSend);
+                            messReply.append("\n")
+                                    .append("[To:")
+                                    .append(message.account.accountId)
+                                    .append("] ")
+                                    .append(message.account.name)
+                                    .append("\n")
+                                    .append(getMessageFromBot(messSend));
+                            System.out.println(messReply.toString());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
 
 // Send message of Bot to the previous sender
                         try {
-                            sendMessage(roomId, "hi");
+                            sendMessage(roomId, messReply.toString());
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
@@ -119,26 +131,140 @@ public class ChatworkClient {
      * @param text
      * @return message
      */
-    public String getMessageFromBot(String text) {
-        String message = StringUtils.EMPTY;
-        if (StringUtils.isBlank(text)) {
-            return message;
+    public String getMessageFromBot(String text) throws IOException, JSONException {
+//        String message = StringUtils.EMPTY;
+//        if (StringUtils.isBlank(text)) {
+//            return message;
+//        }
+//
+//        try {
+//            String json = get(CB_API_URL.replace("%KEY%", CB_API_KEY)
+//                            .replace("%TEXT%", URLEncoder.encode(text, "UTF-8")),
+//                    CB_HEADER_NAME, CB_API_KEY);
+//
+//            String[] results = json.split(",");
+//
+//            message = results[0].split(":")[1];
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return StringUtils.EMPTY;
+//        }
+//
+//        return StringUtils.remove(message, "\"");
+
+        URL url = new URL("https://wsapi.simsimi.com/190410/talk");
+
+
+
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-Type", "application/json");
+        conn.setRequestProperty("x-api-key", "AXN38FpcSNgiJzNwnlnhEKJzdLCHyLPoI+AeTVkZ");
+        JSONObject jsonParam = new JSONObject();
+        jsonParam.put("utext", text);
+        jsonParam.put("lang", "vi");
+//        conn.setDoOutput(true);
+
+//        String jsonInputString = "{\"utext\": \"hi\", \"lang\": \"vi\"}";
+//        try(OutputStream os = conn.getOutputStream()) {
+//            byte[] input = jsonInputString.getBytes("utf-8");
+//            os.write(input, 0, input.length);
+//        }
+
+        try(BufferedReader br = new BufferedReader(
+                new InputStreamReader(conn.getInputStream(), "utf-8"))) {
+            StringBuilder response = new StringBuilder();
+            String responseLine = null;
+            while ((responseLine = br.readLine()) != null) {
+                response.append(responseLine.trim());
+            }
+            System.out.println("aaaaaaaaaa"+response.toString());
         }
 
-        try {
-            String json = get(CB_API_URL.replace("%KEY%", CB_API_KEY)
-                            .replace("%TEXT%", URLEncoder.encode(text, "UTF-8")),
-                    CB_HEADER_NAME, CB_API_KEY);
 
-            String[] results = json.split(",");
+//        DataOutputStream os = new DataOutputStream(conn.getOutputStream());
+//        os.writeBytes(jsonParam.toString());
+//        InputStreamReader isr = new InputStreamReader(conn.getInputStream(), "UTF-8");
+//        BufferedReader br = new BufferedReader(isr);
+//        StringBuilder builder = new StringBuilder();
+//        String line = null;
+//        while ((line = br.readLine()) != null) {
+//            builder.append(line);
+//        }
+//        System.out.println(builder.toString());
+//
+//        br.close();
+//
+//        os.flush();
+//        os.close();
 
-            message = results[0].split(":")[1];
-        } catch (Exception e) {
-            e.printStackTrace();
-            return StringUtils.EMPTY;
-        }
+//        JSONObject jsonObject = new JSONObject();
+//        jsonObject.put("utext", text);
+//        jsonObject.put("lang", "vi");
+//
+//        OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+//        wr.write(jsonObject.toString());
+//        wr.flush();
 
-        return StringUtils.remove(message, "\"");
+//        try (OutputStream os = conn.getOutputStream()) {
+//            byte[] input = jsonInputString.getBytes("utf-8");
+//            os.write(input, 0, input.length);
+//        }
+
+//        try (BufferedReader br = new BufferedReader(
+//                new InputStreamReader(conn.getInputStream(), "utf-8"))) {
+//            StringBuilder response = new StringBuilder();
+//            String responseLine = null;
+//            while ((responseLine = br.readLine()) != null) {
+//                response.append(responseLine.trim());
+//            }
+//            System.out.println("////////////" + response.toString());
+//        }
+
+//        Reader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+//
+//        // Get the response body output from the server
+//        StringBuilder calculatedOutput = new StringBuilder();
+//        String rawOutputLine;
+//        if (in.ready()) {
+//            while ((rawOutputLine = ((BufferedReader) in).readLine()) != null) {
+//                calculatedOutput.append(rawOutputLine);
+//            }
+//
+//            System.out.println("++++++++" + calculatedOutput.toString());
+//            in.close();
+//
+//        }
+//        JSONObject jsonObject1 = new JSONObject(calculatedOutput.toString());
+//        System.out.println("---------"+jsonObject1);
+
+//        GetMethod method = null;
+//        try {
+//            method = new GetMethod(path);
+//            method.addRequestHeader(headerName, apiKey);
+//
+//            int statusCode = httpClient.executeMethod(method);
+//
+//            System.out.println(statusCode+"");
+//
+//            System.out.println( method.getResponseBodyAsString());
+//
+//            if (statusCode != HttpStatus.SC_OK) {
+//                throw new Exception("Response is not valid. Check your API Key or ChatWork API status. response_code = "
+//                        + statusCode + ", message =" + method.getResponseBodyAsString());
+//            }
+//
+//            return method.getResponseBodyAsString();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return StringUtils.EMPTY;
+//        } finally {
+//            if (method != null) {
+//                method.releaseConnection();
+//            }
+//        }
+        return "hhhh";
     }
 
     /**
@@ -158,7 +284,7 @@ public class ChatworkClient {
             }
 
             return mapper.readValue(json, new TypeReference<List<Message>>() {});
-        } catch (IOException e) {
+        } catch (IOException | JSONException e) {
             e.printStackTrace();
             return Collections.EMPTY_LIST;
         }
@@ -201,75 +327,76 @@ public class ChatworkClient {
      * @return json result
      * @throws IOException
      */
-    private String get(String path, String headerName, String apiKey) throws IOException {
+    private String get(String path, String headerName, String apiKey) throws IOException, JSONException {
 
 
-        URL url = new URL("https://wsapi.simsimi.com/190410/talk");
-
-        String jsonInputString = "{\"utext\": \"hi\", \"lang\": \"vi\"}";
-
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-
-        conn.setRequestMethod("POST");
-        conn.setRequestProperty("Content-Type", "application/json");
-        conn.setRequestProperty("x-api-key", "AXN38FpcSNgiJzNwnlnhEKJzdLCHyLPoI+AeTVkZ");
-        conn.setDoOutput(true);
-
-        try (OutputStream os = conn.getOutputStream()) {
-            byte[] input = jsonInputString.getBytes("utf-8");
-            os.write(input, 0, input.length);
-        }
-
-        try (BufferedReader br = new BufferedReader(
-                new InputStreamReader(conn.getInputStream(), "utf-8"))) {
-            StringBuilder response = new StringBuilder();
-            String responseLine = null;
-            while ((responseLine = br.readLine()) != null) {
-                response.append(responseLine.trim());
-            }
-            System.out.println("////////////" + response.toString());
-        }
-
-        Reader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
-
-        // Get the response body output from the server
-        StringBuilder calculatedOutput = new StringBuilder();
-        String rawOutputLine;
-        if (in.ready()) {
-            while ((rawOutputLine = ((BufferedReader) in).readLine()) != null) {
-                calculatedOutput.append(rawOutputLine);
-            }
-            System.out.println("++++++++" + calculatedOutput.toString());
-            in.close();
-
-        }
-
-
-//        GetMethod method = null;
-//        try {
-//            method = new GetMethod(path);
-//            method.addRequestHeader(headerName, apiKey);
+//        URL url = new URL("https://wsapi.simsimi.com/190410/talk");
 //
-//            int statusCode = httpClient.executeMethod(method);
+//        String jsonInputString = "{\"utext\": \"hi\", \"lang\": \"vi\"}";
 //
-//            System.out.println(statusCode+"");
+//        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 //
-//            System.out.println( method.getResponseBodyAsString());
+//        conn.setRequestMethod("POST");
+//        conn.setRequestProperty("Content-Type", "application/json");
+//        conn.setRequestProperty("x-api-key", "AXN38FpcSNgiJzNwnlnhEKJzdLCHyLPoI+AeTVkZ");
+//        conn.setDoOutput(true);
 //
-//            if (statusCode != HttpStatus.SC_OK) {
-//                throw new Exception("Response is not valid. Check your API Key or ChatWork API status. response_code = "
-//                        + statusCode + ", message =" + method.getResponseBodyAsString());
-//            }
-//
-//            return method.getResponseBodyAsString();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            return StringUtils.EMPTY;
-//        } finally {
-//            if (method != null) {
-//                method.releaseConnection();
-//            }
+//        try (OutputStream os = conn.getOutputStream()) {
+//            byte[] input = jsonInputString.getBytes("utf-8");
+//            os.write(input, 0, input.length);
 //        }
-        return "cu";
+//
+//        try (BufferedReader br = new BufferedReader(
+//                new InputStreamReader(conn.getInputStream(), "utf-8"))) {
+//            StringBuilder response = new StringBuilder();
+//            String responseLine = null;
+//            while ((responseLine = br.readLine()) != null) {
+//                response.append(responseLine.trim());
+//            }
+//            System.out.println("////////////" + response.toString());
+//        }
+//
+//        Reader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+//
+//        // Get the response body output from the server
+//        StringBuilder calculatedOutput = new StringBuilder();
+//        String rawOutputLine;
+//        if (in.ready()) {
+//            while ((rawOutputLine = ((BufferedReader) in).readLine()) != null) {
+//                calculatedOutput.append(rawOutputLine);
+//            }
+//
+//            System.out.println("++++++++" + calculatedOutput.toString());
+//            in.close();
+//
+//        }
+//        JSONObject jsonObject = new JSONObject(calculatedOutput.toString());
+//        System.out.println(jsonObject+"");
+
+        GetMethod method = null;
+        try {
+            method = new GetMethod(path);
+            method.addRequestHeader(headerName, apiKey);
+
+            int statusCode = httpClient.executeMethod(method);
+
+            System.out.println(statusCode+"");
+
+            System.out.println( method.getResponseBodyAsString());
+
+            if (statusCode != HttpStatus.SC_OK) {
+                throw new Exception("Response is not valid. Check your API Key or ChatWork API status. response_code = "
+                        + statusCode + ", message =" + method.getResponseBodyAsString());
+            }
+
+            return method.getResponseBodyAsString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return StringUtils.EMPTY;
+        } finally {
+            if (method != null) {
+                method.releaseConnection();
+            }
+        }
     }
 }
